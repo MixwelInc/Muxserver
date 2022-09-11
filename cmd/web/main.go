906f6da66967f6439a835e8e59a31cc717edb151
snuser <smakar20001@gmail.com>
2022-09-11 23:17:48 +0300

    added work with static, logs and so on

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "Web address HTTP")
+	flag.Parse()
+	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime) //creating log for info messages
+	errlog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Запуск сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))         // accessing static files
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer)) // removing "/static"
+
+	srv := &http.Server{ //initializing the server (the only change is redirecting errors to created errlog)
+		Addr:     *addr,
+		ErrorLog: errlog,
+		Handler:  mux,
+	}
+
+	infolog.Printf("Запуск сервера на %s", *addr)
+	err := srv.ListenAndServe()
+	errlog.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat() // check index.html file in the path
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+	return f, nil
 }
